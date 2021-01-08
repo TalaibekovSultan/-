2e6920724768549624cebde7c297a1c13a06be3d
TalaibekovSultan <76248095+TalaibekovSultan@users.noreply.github.com>
2021-01-08 19:36:13 +0600

    Update users.py

--- users.py
+++ users.py
@@ -8,12 +8,12 @@ with open(file_name, 'w') as f:
 i = 0
 while i == 0:
     print("Здраствуйте", name)
-    print("a) Посмотореть свои данные:")
-    print("б) Изменить свои данные:")
-    print("в) Вывод файлов с пользователями:")
-    print("г) Закрыть программу:")
+    print("1) Посмотореть свои данные:")
+    print("2) Изменить свои данные:")
+    print("3) Вывод файлов с пользователями:")
+    print("4) Закрыть программу:")
     read = input("Выберите: ")
-    if read == 'а':
+    if read == '1':
         print('Вот ваши данные: ', user_data)
         a = input("Желаете изменить свои данные?: ")
         if a == 'да':
@@ -22,25 +22,25 @@ while i == 0:
                 s = open(file_name, 'w')
                 s.write(user_data)
                 s.close()
-    elif read == 'б':
+    elif read == '2':
         user_data = input('Введите свои данные: ')
         with open(file_name, 'w') as f:
             s = open(file_name, 'w')
             s.write(user_data)
             s.close()
-    elif read == 'в':
+    elif read == '3':
         import os
 
         directory = os.listdir()
+
         for index, file in enumerate(directory):
             print(index, ':', file)
         x = input('Хотите-ли посмотреть содержимое какого либо файла?: ')
         if x == 'да':
-            y = input('Введите номер файла: ')
-
-
-
-    elif read == 'г':
+            y = int(input('Введите номер файла: '))
+            with open(directory[y], 'r') as f:
+                print('readline: ', f.readlines())
+    elif read == '4':
         i = i + 1
     else:
         print("Что-то пошло не так")
